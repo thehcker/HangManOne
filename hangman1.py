f8c8f3ca726c467a4e476c0438a448a8a0f9d59e
thehcker <isaacnyakoi1@gmail.com>
2018-09-11 18:40:26 +0300

    Hangman1 readlines() method

--- hangman1.py
+++ hangman1.py
@@ -1,6 +1,6 @@
 from random import choice
 def pick_random_word():
 	f = open('sowpods.txt', 'r')
-	line = f.read().split('\n')
+	line = f.readlines()			# Or use line = f.read().split('\n')
 	return choice(line)
 print(pick_random_word())
